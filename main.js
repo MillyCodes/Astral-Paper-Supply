66a6b47b4b44d0e62e9cdd619a95effed175bb4d
Mildred Rodriguez <millz118@gmail.com>
2018-07-19 11:55:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,32 +5,38 @@ let secondSection = sections[1]
 let thirdSection = sections[2]
 let fourthSection = sections[3]
 let fifthSection = sections[4]
-let lastSection = sections[5]
+let sixthSection = sections[5]
+let lastSection = sections[6]
 
 // run this code on window scroll
 window.addEventListener("scroll", (event) => {
 
-  if (window.scrollY > 50) {
+  if (window.scrollY > 1) {
     firstSection.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 300) {
+  if (window.scrollY > 200) {
     secondSection.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 800) {
+  if (window.scrollY > 500) {
     thirdSection.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 1100) {
+  if (window.scrollY > 900) {
     fourthSection.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 1500) {
+  if (window.scrollY > 1100) {
     fifthSection.classList.add("fadeIn")
   }
 
-  if (window.scrollY > 1600) {
+
+  if (window.scrollY > 1100) {
+    sixthSection.classList.add("fadeIn")
+  }
+
+  if (window.scrollY > 1400) {
     lastSection.classList.add("fadeIn")
   }
 })
